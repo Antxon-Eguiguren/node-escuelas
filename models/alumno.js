030b3c1eb917939295ccee3b34651a98e16edbda
Antxon Eguiguren <antxonegi@gmail.com>
2020-03-03 22:50:34 +0100

    Función addStudent añadida

--- models/alumno.js
+++ models/alumno.js
@@ -1,12 +1,22 @@
 const getAll = () => {
     return new Promise((resolve, reject) => {
-        db.query('select * from alumnos', (err, rows) => {
+        db.query(`SELECT * FROM alumnos`, (err, rows) => {
             if (err) reject(err);
             resolve(rows);
         });
     });
 };
 
+const addStudent = (pNombre, pApellidos, pEdad, pEmail, pNotaMedia, pFechaMat, pMatricula, pDiscapacidad, pSexo) => {
+    return new Promise((resolve, reject) => {
+        db.query(`INSERT INTO alumnos (nombre, apellidos, edad, email, notamedia, fecha_matricula, matricula, discapacidad, sexo) VALUES (${pNombre}, ${pApellidos}, ${pEdad}, ${pEmail}, ${pNotaMedia}, ${pFechaMat}, ${pMatricula}, ${pDiscapacidad}, ${pSexo})`, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
 module.exports = {
-    getAll: getAll
+    getAll: getAll,
+    addStudent: addStudent
 }
--- routes/students.js
+++ routes/students.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const moment = require('moment');
-
 
 const Alumno = require('../models/alumno');
 
 // GET http://localhost:3000/students
 router.get('/', async (req, res) => {
-    const rows = await Alumno.getAll();
-    res.render('../views/escuela/index', { rows });
+    try {
+        const rows = await Alumno.getAll();
+        res.render('../views/escuela/index', { rows });
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
+
+// ¿CÓMO SE HACE CON THEN Y CATCH?
+// router.get('/', (req, res) => {
+//     Alumno.getAll()
+//         .then(res.render('../views/escuela/index', { rows }))
+//         .catch(console.log(err));
+// });
 
 // GET http://localhost:3000/students/new
 router.get('/new', (req, res) => {
@@ -17,16 +27,15 @@ router.get('/new', (req, res) => {
 });
 
 // POST http://localhost:3000/students/create
-router.post('/create', (req, res) => {
-    const student = {
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        edad: req.body.edad,
-        email: req.body.email,
-        numMat: req.body.numMat
+router.post('/create', async (req, res) => {
+    try {
+        await Alumno.addStudent(req.body.nombre, req.body.apellidos, req.body.edad, req.body.email, req.body.notamedia, req.body.fecha_matricula, req.body.matricula, req.body.discapacidad, req.body.sexo);
+        console.log('Alumno creado correctamente');
+        res.redirect('/');
     }
-    students.push(student);
-    res.render('../views/escuela/newStudent', { student });
+    catch (err) {
+        console.log(err);
+    }
 });
 
 // GET http://localhost:3000/students/edit
